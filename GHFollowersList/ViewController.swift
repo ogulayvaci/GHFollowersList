dd69bfe29c9c069635b1c0bbbe6aa8f11b1b18a2
Ogul Ayvaci <h.ogulayvaci@gmail.com>
2025-02-15 00:31:44 +0300

    Initial Commit

---
+++ GHFollowersList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowersList
+//
+//  Created by Ogul Ayvaci on 15.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
