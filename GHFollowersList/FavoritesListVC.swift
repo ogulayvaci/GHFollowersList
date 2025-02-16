76e113834d2f974ae0c899760204e121abf43405
Ogul Ayvaci <h.ogulayvaci@gmail.com>
2025-02-16 16:13:21 +0300

    Initial commit: SearchVC and FollowerListVC added. GFButton and GFTextField created. Icons added.

---
+++ GHFollowersList/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowersList
+//
+//  Created by Ogul Ayvaci on 15.02.2025.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
