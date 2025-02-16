0c89ef6eb8ffe4e2ad57f84a80665c16196830bb
Ogul Ayvaci <h.ogulayvaci@gmail.com>
2025-02-16 23:02:37 +0300

    GFAvatarImageView and FollowerCell for UICollectionView added.

--- GHFollowersList/Screens/FollowerListVC.swift
+++ GHFollowersList/Screens/FollowerListVC.swift
@@ -10,22 +10,14 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!.rawValue, buttonTitle: "OK")
-                return
-            }
-            
-            print("Followers.count = \(followers.count)")
-            print(followers)
-        }
+        configureCollectionView()
+        configureViewController()
+        getFollowers()
     }
     
     
@@ -33,5 +25,54 @@ class FollowerListVC: UIViewController {
         super.viewWillAppear(animated)
         
         navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    
+    func configureViewController(){
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+    
+    
+    func configureCollectionView(){
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        
+        view.addSubview(collectionView)
+        
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (2 * padding) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+        
+    
+    
+    func getFollowers(){
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+                
+            case .success(let followers):
+                print("Followers.count = \(followers.count)")
+                print(followers)
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "OK")
+            }
+        }
     }
 }
