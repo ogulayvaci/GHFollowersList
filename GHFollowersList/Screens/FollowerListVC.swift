45b7ba15258d0dd929b261522f638a92f0c35534
Ogul Ayvaci <h.ogulayvaci@gmail.com>
2025-02-18 19:21:34 +0300

    FollowerListVC completed. GFEmptyStateView added. SearchBar added.

--- GHFollowersList/Screens/FollowerListVC.swift
+++ GHFollowersList/Screens/FollowerListVC.swift
@@ -9,21 +9,32 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
+    enum Section{
+        case main
+    }
+    
     var username: String!
+    var followers: [Follower] = []
+    var filteredFollowers: [Follower] = []
+    var page = 1
+    var hasMoreFollower = true
+    
     var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureCollectionView()
         configureViewController()
-        getFollowers()
+        configureSearchController()
+        configureCollectionView()
+        getFollowers(username: username, page: page)
+        configureDataSource()
     }
     
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
@@ -36,43 +47,110 @@ class FollowerListVC: UIViewController {
     
     
     func configureCollectionView(){
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         
         view.addSubview(collectionView)
-        
-        collectionView.backgroundColor = .systemPink
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
     
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth = width - (2 * padding) - (minimumItemSpacing * 2)
-        let itemWidth = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        return flowLayout
+    func configureSearchController() {
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.autocapitalizationType = .none
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Search followers..."
+        navigationItem.searchController = searchController
     }
+    
+    
+    
+    func getFollowers(username: String, page: Int){
         
-    
-    
-    func getFollowers(){
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        showLoadingView()
+        
+        NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
+            
+            guard let self = self else { return }
+            
+            self.dismissLoadingView()
             
             switch result {
-                
             case .success(let followers):
-                print("Followers.count = \(followers.count)")
-                print(followers)
+                if followers.count < 100 {
+                    self.hasMoreFollower = false
+                }
+                
+                self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user has no followers."
+                    
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                    }
+                    return
+                }
+                self.updateData(on: self.followers)
                 
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "OK")
             }
         }
+    }
+    
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    
+    func updateData(on followers: [Follower]){
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+    }
+}
+
+
+extension FollowerListVC: UICollectionViewDelegate {
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        if offsetY > contentHeight - height {
+            guard hasMoreFollower else { return }
+            
+            page += 1
+            getFollowers(username: username, page: page)
+        }
+    }
+}
+
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate{
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        
+        // when user types "s", included ones will be added on a array.
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
+        updateData(on: filteredFollowers)
+    }
+    
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        updateData(on: followers)
     }
 }
